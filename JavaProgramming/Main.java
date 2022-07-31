77f836e763c7a6f12435bf3b78db027d6bd3f60e
shakurphilemon <47128432+shakurphilemon@users.noreply.github.com>
2022-07-31 17:23:22 +0200

    Add files via upload

---
+++ JavaProgramming/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    static void myMethod(){
+        System.out.println("This is my program called method");
+
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+
+    }
+}
